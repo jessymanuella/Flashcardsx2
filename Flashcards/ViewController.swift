2b51c9366e679e55eccab0b9dd7b0b694ff6ce71
Jessy Manuella <jessytientcheu@rocketmail.com>
2020-02-14 11:47:14 -0500

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Jessy Manuella on 2/14/20.
+//  Copyright © 2020 Jessy Manuella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
